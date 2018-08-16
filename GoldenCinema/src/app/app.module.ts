339e93f33fc80a644e2d5ecb501fd7ff265189d2
Ivan Zhang <yufei.z222@gmail.com>
2018-08-16 18:45:58 +1000

    Add navigation bar and search box

--- GoldenCinema/src/app/app.module.ts
+++ GoldenCinema/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule
