c717bb848d502815e1e8c883ec5d74fa0e616b38
Bao Trang Bui <crysbui.depon@gmail.com>
2018-08-27 18:07:12 +1000

    Modify all paths affected by these changes in app.module.ts and routing.module.ts

--- GoldenTimeCinema/src/app/routing.module.ts
+++ GoldenTimeCinema/src/app/routing.module.ts
@@ -1,62 +1,60 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { GenreComponent } from "./genre/genre.component";
-import { HomeComponent } from "./home/home.component";
-import { AwardComponent } from "./award/award.component";
-import { CompanyInformationComponent } from "./company-information/company-information.component";
-import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
-import { AddMovieFormComponent } from "./add-movie-form/add-movie-form.component";
-import { LoginComponent } from "./login/login.component";
-import { RegisterComponent } from "./register/register.component";
-import { EditMovieFormComponent } from "./edit-movie-form/edit-movie-form.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { GenreComponent } from "./components/genre/genre.component";
+import { HomeComponent } from "./components/home/home.component";
+import { AwardComponent } from "./components/award/award.component";
+import { CompanyInformationComponent } from "./components/company-information/company-information.component";
+import { NotFoundPageComponent } from "./components/not-found-page/not-found-page.component";
+import { AddMovieFormComponent } from "./components/add-movie-form/add-movie-form.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { EditMovieFormComponent } from "./components/edit-movie-form/edit-movie-form.component";
 
 const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-    },
-    {
-        path: "home",
-        component: HomeComponent
-    },
-    {
-        path: "genre",
-        component: GenreComponent
-    },
-    {
-        path: "award",
-        component: AwardComponent
-    },
-    {
-        path: "companyInformation",
-        component: CompanyInformationComponent
-    },
-    {
-        path: "addMovieForm",
-        component: AddMovieFormComponent
-    },
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "register",
-        component: RegisterComponent
-    },
-    {
-        path: "editMovieForm",
-        component: EditMovieFormComponent
-    },
-    {
-        path: "**",
-        component: NotFoundPageComponent
-    }
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "genre",
+    component: GenreComponent
+  },
+  {
+    path: "award",
+    component: AwardComponent
+  },
+  {
+    path: "companyInformation",
+    component: CompanyInformationComponent
+  },
+  {
+    path: "addMovieForm",
+    component: AddMovieFormComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "editMovieForm",
+    component: EditMovieFormComponent
+  },
+  {
+    path: "**",
+    component: NotFoundPageComponent
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [
-        RouterModule
-    ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class RoutingModule { };
+export class RoutingModule {}
