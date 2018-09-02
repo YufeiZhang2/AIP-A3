0dac133cc0e3ec03ba01c4cade8a5bad42895a5a
baotrangbui <crysbui.depon@gmail.com>
2018-09-03 03:03:37 +1000

    Add front-end validation for login and register function (test with essential input of email and password only)

---
+++ GoldenTimeCinema-angular/src/app/components/authentication/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "authentication",
+  templateUrl: "./authentication.component.html"
+})
+export class AuthenticationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ GoldenTimeCinema-angular/src/app/components/authentication/logout.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "logout",
+  templateUrl: "./logout.component.html"
+})
+export class LogoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onLogout() {}
+}
