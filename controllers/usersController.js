8791dad9735a9736a46c2928a59685f79fd50c42
BaoTrang Bui <crysbui.depon@gmail.com>
2018-09-17 13:45:29 +1000

    Add new properties for user on server side
    Add other properties (fullName, lastName, gender, dob) to userModel and userController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,8 +6,12 @@ const User = mongoose.model("user");
 
 module.exports.register = (req, res, next) => {
   var user = new User();
+  user.firstName = req.body.firstName;
+  user.lastName = req.body.lastName;
   user.email = req.body.email;
   user.password = req.body.password;
+  user.gender = req.body.gender;
+  user.dob = req.body.dob;
   user.save((err, doc) => {
     if (!err) res.send(doc);
     else {
@@ -42,47 +46,3 @@ module.exports.userProfile = (req, res, next) => {
         .json({ status: true, user: _.pick(user, ["email"]) });
   });
 };
-
-// const auth = require("../middleware/auth");
-// const jwt = require("jsonwebtoken");
-// const config = require("config");
-// const bcrypt = require("bcrypt");
-// const express = require("express");
-// const router = express.Router();
-// const { userModel, validate } = require("../models/userModel");
-// const _ = require("lodash");
-
-// // get the current user
-// // check authorization
-// router.get("/me", auth, async (req, res) => {
-//   const user = await User.findById(req.user._id).select("-password"); // exclude password in request sent back to client
-//   res.send(user);
-// });
-
-// //register a new user
-// router.post("/", async (req, res) => {
-//   const { error } = validate(req.body);
-//   if (error) return res.status(400).send(error.details[0].message);
-
-//   //check if there is existing user in the database
-//   let user = await userModel.findOne({ email: req.body.email });
-//   if (user) return res.status(400).send("User already registered");
-
-//   //create a new user and using lodash to pick certain properties to store in the database
-//   user = new User(_.pick(req.body, ["email", "password"]));
-
-//   // encrypt plain text password using bcrypt to generate a salt and attach salt to new hashed password
-//   const salt = await bcrypt.genSalt(10);
-//   user.password = await bcrypt.hash(user.password, salt);
-
-//   //save a new user
-//   await user.save();
-
-//   const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey")); //set the payload of jwt
-//   //using lodash to return certain properties to the client with attached header of jwt
-//   res
-//     .header("x-auth-token", token)
-//     .send(_.pick(user, ["_id", "name", "email"]));
-// });
-
-// module.exports = router;
--- models/userModel.js
+++ models/userModel.js
@@ -3,6 +3,14 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 var userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: "First name is required"
+  },
+  lastName: {
+    type: String,
+    required: "Last name is required"
+  },
   email: {
     type: String,
     required: "Email is required",
@@ -11,7 +19,16 @@ var userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: "Password is required",
-    minlength: [4, "Password must have at least 4 characters"]
+    minlength: [6, "Password must have at least 4 characters"]
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+    required: "Gender is required"
+  },
+  dob: {
+    type: Date,
+    required: "Date of birth is required"
   },
   saltSecret: String
 });
@@ -46,61 +63,3 @@ userSchema.methods.generateJwt = function() {
 };
 
 mongoose.model("user", userSchema);
-
-// const config = require("config");
-// const jwt = require("jsonwebtoken");
-// const Joi = require("joi");
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//   name: { type: String, required: true, minlength: 2, maxlength: 100 },
-//   gender: {
-//     type: String,
-//     enum: ["male", "female", "intersex", "secret"],
-//     required: true
-//   },
-//   DOB: Date,
-//   password: { type: String, required: true, minlength: 6, maxlength: 20 },
-//   email: {
-//     type: String,
-//     unique: true,
-//     required: true,
-//     minlength: 5,
-//     maxlength: 1024
-//   },
-//   isAdmin: Boolean
-// });
-
-// userSchema.methods.generateAuthToken = function() {
-//   // generate a json web token when user sign in with selected properties in the payload of jwt
-//   const token = jwt.sign(
-//     { _id: this._id, isAdmin: this.isAdmin },
-//     config.get("jwtPrivateKey")
-//   );
-//   return token;
-// };
-
-// const userModel = mongoose.model("user", userSchema);
-
-// function validateUser(user) {
-//   const schema = {
-//     name: Joi.string()
-//       .min(2)
-//       .max(100)
-//       .required(),
-//     email: Joi.string()
-//       .min(5)
-//       .max(255)
-//       .required()
-//       .email(),
-//     gender: Joi.string().required(),
-//     password: Joi.string()
-//       .min(6)
-//       .max(255)
-//       .required()
-//   };
-//   return Joi.validate(user, schema);
-// }
-
-// exports.userModel = userModel;
-// exports.validate = validateUser;
