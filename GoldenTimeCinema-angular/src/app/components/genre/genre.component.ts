d8d3388dd7f1348b8bf3306db0a32c82239a7668
Ivan Zhang <yufei.z222@gmail.com>
2018-10-01 20:57:03 +1000

    add search movies by genres in genre page

--- GoldenTimeCinema-angular/src/app/components/genre/genre.component.ts
+++ GoldenTimeCinema-angular/src/app/components/genre/genre.component.ts
@@ -1,3 +1,4 @@
+import { MoviesService } from './../../services/movies.service';
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -6,7 +7,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./genre.component.css"]
 })
 export class GenreComponent implements OnInit {
-  constructor() {}
+  dramas = "dramas";
+  action = "action";
+  crime = "crime";
+  horror = "horror";
+  comedies = "comedies"
+  fantasy = "fantasy";
+  romance = "romance";
+  movies: any[];
+  movieResult: any[];
 
-  ngOnInit() {}
+  constructor(private service: MoviesService) { }
+
+  ngOnInit() { }
+
+  findGenre(value) {
+    console.log(value);
+    this.service.getMovies().subscribe(response => {
+      this.movies = response.json();
+
+      for (let movie of this.movies)
+        for (let genre of movie.genres) {
+
+          if (this.CompareWithWords(genre, value)) {
+            //find all the movies with the same genre
+            console.log(movie._id, movie.name);
+            this.service.getMoviesById(movie._id).subscribe(response => {
+              //get the movies matched by genres
+              this.movieResult = response.json();
+            })
+
+          }
+        }
+
+
+    })
+  }
+
+  //method for verifying if two words match without whitespace
+  CompareWithWords(firstWord, secondWord) {
+    //remove all the whitespace of strings
+    const firstResult = firstWord.toString().toLowerCase().trim().replace(/ /g, '');
+    const secondResult = secondWord.toString().toLowerCase().trim().replace(/ /g, '');
+
+    return firstResult === secondResult ? true : false;
+  }
 }
+
+
