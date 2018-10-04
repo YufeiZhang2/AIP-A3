118ee7c2404cd9252add520d734e74bff6c8e62a
BaoTrang Bui <crysbui.depon@gmail.com>
2018-10-04 18:28:01 +1000

    Refactor user api for consistency with movie api

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("./models/userModel");
 require("./config/passportConfig");
 require("./config/config");
 
-const routesApp = require("./routes/appRouter");
 const passport = require("passport");
 const express = require("express");
 const path = require("path");
@@ -10,6 +9,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dbConfig = require("./config/cinemaDb");
 const moviesController = require("./controllers/moviesController");
+const usersController = require("./controllers/usersController");
 
 // Connect mongoose to our database
 mongoose
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Set api routes
 app.use("/api/movies", moviesController);
 app.use("/api/movies/:_id", moviesController);
-app.use("/api", routesApp);
+app.use("/api", usersController);
 
 // Handle errors for validation
 app.use((err, req, res, next) => {
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/usersController");
+const jwtHelper = require("../config/jwtHelper");
+
 const mongoose = require("mongoose");
 const passport = require("passport");
 const _ = require("lodash");
@@ -47,3 +53,13 @@ module.exports.userProfile = (req, res, next) => {
       });
   });
 };
+
+router.post("/register", userController.register);
+router.post("/authenticate", userController.authenticate);
+router.get(
+  "/userProfile",
+  jwtHelper.verifyJwtToken,
+  userController.userProfile
+);
+
+module.exports = router;
