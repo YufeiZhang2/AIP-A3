8a8a6d93cf722ad755c28b9128d195e6fa9a8bf7
Ivan Zhang <yufei.z222@gmail.com>
2018-10-19 20:55:45 +1100

    refactoring movie.service

--- GoldenTimeCinema-angular/src/app/services/movies.service.ts
+++ GoldenTimeCinema-angular/src/app/services/movies.service.ts
@@ -6,28 +6,31 @@ import { environment } from "../../environments/environment";
   providedIn: "root"
 })
 export class MoviesService {
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   private url = environment.apiBaseUrl + "/movies";
 
-  // Link to api in movieController
+  // get all the movies 
   getMovies() {
     return this.http.get(this.url);
   }
 
+  // get the movie by its object id
   getMoviesById(_id) {
     return this.http.get(this.url + "/" + _id);
   }
 
+  //post a new movie
   createMovies(movie) {
     return this.http.post(this.url, movie);
   }
 
+  //update a movie's status
   updateMovies(movie) {
-    console.log("in service put", movie);
     return this.http.put(this.url + "/" + movie._id, movie);
   }
 
+  //delete a movie by its object id
   deleteMovies(_id) {
     return this.http.delete(this.url + "/" + _id);
   }
