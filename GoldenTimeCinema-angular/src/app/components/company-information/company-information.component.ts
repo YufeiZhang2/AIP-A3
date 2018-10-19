3d0ed6500679db6ab566b5107e0a795da8169f97
Ivan Zhang <yufei.z222@gmail.com>
2018-10-19 19:51:42 +1100

    refactoring company-information-component

--- GoldenTimeCinema-angular/src/app/components/company-information/company-information.component.ts
+++ GoldenTimeCinema-angular/src/app/components/company-information/company-information.component.ts
@@ -5,8 +5,8 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./company-information.component.html",
   styleUrls: ["./company-information.component.css"]
 })
-export class CompanyInformationComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class CompanyInformationComponent {
+  introduction: string = `Golden Time Cinema is for movie lovers to enjoy the classis movies anytime. Our cinema contains more than ten movie rooms
+  with cozy enviroment. We hope audience can have the best experince and travel with time machine to the classic movies
+  they love. Feel free to contact us if you have any question. Our team is here to help you responsively.`;
 }
